ee9cb434778bf49dd63fb174f049735156eca31f
kioko91 <calvinkioko91@gmail.com>
2023-07-13 15:28:27 +0300

    jkj

--- app/src/main/java/com/example/airtime/Notifications.java
+++ app/src/main/java/com/example/airtime/Notifications.java
@@ -1,66 +1,56 @@
 package com.example.airtime;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.NotificationCompat;
-
-import android.app.AlarmManager;
-import android.app.NotificationChannel;
-import android.app.NotificationManager;
 import android.app.PendingIntent;
-import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.os.Build;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
 import java.util.Date;
+import java.util.Locale;
 
-public class Notifications extends AppCompatActivity {
-
+public class Notifications {
     private static final String CHANNEL_ID = "My Notification";
+    private static final int NOTIFICATION_ID_1 = 1;
+    private static final int NOTIFICATION_ID_2 = 2;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_notifications);
-        Button button = findViewById(R.id.buttonNot);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, "My Notification", NotificationManager.IMPORTANCE_DEFAULT);
-            NotificationManager manager = getSystemService(NotificationManager.class);
-            manager.createNotificationChannel(channel);
-        }
+    public static void scheduleNotification(Context context, String title, String content, String notificationTime, int notificationId) {
+        long timeInMillis = getTimeInMillis(notificationTime);
+
+        Intent intent = new Intent(context, NotificationReceiver.class);
+        intent.putExtra("title", title);
+        intent.putExtra("content", content);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(context, notificationId, intent, PendingIntent.FLAG_ONE_SHOT);
+
+        Intent fullScreenIntent = new Intent(context, NotificationsActivity.class);
+        fullScreenIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        PendingIntent fullScreenPendingIntent = PendingIntent.getActivity(context, 0, fullScreenIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, CHANNEL_ID)
+                .setContentTitle(title)
+                .setContentText(content)
+                .setSmallIcon(R.drawable.avatar)
+                .setAutoCancel(true)
+                .setContentIntent(pendingIntent)
+                .setFullScreenIntent(fullScreenPendingIntent, true);
+
+        NotificationManagerCompat managerCompat = NotificationManagerCompat.from(context);
+        managerCompat.notify(notificationId, builder.build());
     }
 
-
-        public static void scheduleNotification(Context context, String title, String content, String dateTime, int notificationId) {
-            long notificationTime = getTimeInMillis(dateTime);
-
-            Intent intent = new Intent(context, NotificationReceiver.class);
-            intent.putExtra("title", title);
-            intent.putExtra("content", content);
-            PendingIntent pendingIntent = PendingIntent.getBroadcast(context, notificationId, intent, PendingIntent.FLAG_ONE_SHOT);
-
-            AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
-            if (alarmManager != null) {
-                alarmManager.setExact(AlarmManager.RTC_WAKEUP, notificationTime, pendingIntent);
+    private static long getTimeInMillis(String dateTime) {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy H:mm", Locale.getDefault());
+        try {
+            Date date = sdf.parse(dateTime);
+            if (date != null) {
+                return date.getTime();
             }
+        } catch (ParseException e) {
+            e.printStackTrace();
         }
-
-        private static long getTimeInMillis(String dateTime) {
-            SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy H:mm");
-            try {
-                Date date = sdf.parse(dateTime);
-                Calendar calendar = Calendar.getInstance();
-                calendar.setTime(date);
-                return calendar.getTimeInMillis();
-            } catch (ParseException e) {
-                e.printStackTrace();
-            }
-            return 0;
-        }
+        return 0;
     }
+}
